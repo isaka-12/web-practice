3d274f0c175e85204590625973ebab462842c645
isaka-12 <isakamtweve69@gmail.com>
2025-09-10 18:36:43 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+ document.getElementById("myButton").addEventListener("click", function() {
+        alert("Button was clicked!");
+    });
+    console.log("Hello, World!");
+    document.getElementById("myDiv").innerText = "This text was added by JavaScript.";
+   
+
+    
